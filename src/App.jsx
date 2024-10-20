b445e7b8f7905c88a82412c68810af02c8090df8
ilya-aby <2776078+ilya-aby@users.noreply.github.com>
2024-10-19 22:31:54 -0700

    Randomize dice, add reroll button

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Die from './components/Die.jsx'
 
 export default function App() {
 
+  const allNewDice = () => {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push({value: Math.ceil(Math.random() * 6), isHeld: false})
+    }
+    return newDice
+  }
+
+  const [dice, setDice] = useState(allNewDice())
+
+  const rollDice = () => {
+    setDice(allNewDice())
+  }
+  
   return (
-    <>
-      <main>
+    <main>
+      <div>
         <h1>Tenzies</h1>
         <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-        <div className="dice-container">
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={3} />
-          <Die value={4} />
-          <Die value={5} />
-          <Die value={6} />
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={3} />
-          <Die value={4} />
-        </div>
-      </main>
-    </>
+      </div>
+      <div className="dice-container">
+        {dice.map((die) => (
+          <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+        ))}
+      </div>
+      <button onClick={rollDice}>Roll</button>
+    </main>
   )
 }
